f471d56cd70b425fc20ffbd277bbf5bb9d1f821a
Steve C <stevenalexcarino@gmail.com>
2021-07-04 16:35:47 -0400

    Home page update

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="hello">
-    Welcome to my personal website, I hope you like it. 
-    <br>There's not much here yet
-    
-  <table>
+    Welcome to my personal website! 
+    <br> About me:
+    I dedicated most of my time in high school studying Art, Design and History,
+    and was accepted into a Fine Art Illustration Major at an early age.
+    <br> I decided the Art Industry wasn't for me, deadlines and commercial viability are the antithesis to artistry.
+    <br> What interested me is how software provided uncalculable wealth for artists. 
+    Limitless paper, unlimited clay, silkscreens, etc. 
+    New tools would arrive for specific use cases, and everyone would celebrate on online forums, sharing their configurations.
+    <br> I want to program like an artist, studying and learning from the tools I was given,
+    and contributing meaningfully to the future.
+
+  <!-- <table>
     <tr><td></td><td></td></tr>
     <tr></tr>
     <tr></tr>
-  </table>
+  </table> -->
 
   </div>
 </template>
