2ef0410c6a278660d8e8402b74ab2ed6905b458b
SC <stevenalexcarino@gmail.com>
2025-08-10 12:05:19 -0400

    no tokens

--- src/api.js
+++ src/api.js
@@ -5,11 +5,9 @@ const token = process.env.GITHUB_TOKEN;
 export async function github_userdata (username){
     let userapi = "https://api.github.com/users/" + username
     return axios.get(userapi,{
-        headers: {
-            Authorization: 'Bearer ' + token //the token is a variable which holds the token
-          } 
-
-        
+        // headers: {
+        //     Authorization: 'Bearer ' + token //the token is a variable which holds the token
+        //   }
     }).then((resp) => {
         console.log("userdata: ", resp.data)
         return resp.data;
@@ -19,10 +17,9 @@ export async function github_userdata (username){
 export async function github_repodata (username){
     let repoapi = "https://api.github.com/users/" + username + "/repos"
     return axios.get(repoapi,{
-        headers: {
-            Authorization: 'Bearer ' + token //the token is a variable which holds the token
-          } 
-
+        // headers: {
+        //     Authorization: 'Bearer ' + token //the token is a variable which holds the token
+        //   } 
     }
     ).then((resp) => {
 
