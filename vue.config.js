97c9115b544feffb0150eb1adb345214d9b516aa
Steven C <44567955+stevenaci@users.noreply.github.com>
2025-08-10 11:08:50 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/stevenaci.github.io/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-website/" : "/",
 };
